a7adc222e31526a9ceaed440a2790b2778201fb3
goodGuyVictor <victor.kataev1@gmail.com>
2017-08-10 12:23:12 +0200

    improved logic of defaultcontroller, added error handling condition

--- simpleengine/controllers/defaultcontroller.php
+++ simpleengine/controllers/defaultcontroller.php
@@ -10,23 +10,27 @@ namespace simpleengine\controllers;
 
 
 use simpleengine\core\Application;
+use simpleengine\models\Login;
+use simpleengine\models\User;
 
 class DefaultController extends AbstractController
 {
     public function actionIndex()
     {
         session_start();
-        if(key_exists('email', $_SESSION) && $_SESSION['email'] != "") {
-            $app = Application::instance();
-            $sql = "SELECT * FROM users WHERE email='".$_SESSION['email']."'";
-            $result = $app->db()->getArrayBySqlQuery($sql);
 
-            if(!empty($result)) {
-                $firstname = $result[0]['firstname'];
-                echo $this->render('index_loggedin', [
-                    "firstname" => $firstname
-                ]);
-            } else echo "fail bro";
+        if(key_exists('error', $_SESSION) && $_SESSION['error'] != '') {
+            header("Location: /default/login/");
+            exit;
+        }
+
+        if(key_exists('email', $_SESSION) && $_SESSION['email'] != "") {
+            $loggedInUser = new Login($_SESSION['email'], '');
+            $userId = $loggedInUser->getIdByEmail();
+            $user = new User($userId);
+            echo $this->render('index_loggedin', [
+                "firstname" => $user->getFirstname()
+            ]);
         } else {
             echo $this->render("index");
         }
@@ -34,7 +38,7 @@ class DefaultController extends AbstractController
     }
 
     public function actionLogin(){
-
+        echo "actionLogin";
     }
 
 
