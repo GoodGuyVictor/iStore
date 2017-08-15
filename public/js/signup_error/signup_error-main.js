4e70a62831aba9d63e3fa977c963ce5073f2f269
goodGuyVictor <victor.kataev1@gmail.com>
2017-08-15 16:36:02 +0200

    signup_error is ready

---
+++ public/js/signup_error/signup_error-main.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Victor on 8/15/2017.
+ */
+$(document).ready(function() {
+
+    function getEmptyFields() {
+        var firstname = $('#firstname').val();
+        var lastname = $('#lastname').val();
+        var email = $('#email').val();
+        var password = $('#password').val();
+        var result = [];
+
+        if(!firstname)
+            result.push('First name');
+        if(!lastname)
+            result.push('Last name');
+        if(!email)
+            result.push('Email');
+        if(!password)
+            result.push('Password');
+
+        return result;
+    }
+
+    $('#signup').on('click', function () {
+        var firstname = $('#firstname').val();
+        var lastname = $('#lastname').val();
+        var email = $('#email').val();
+        var password = $('#password').val();
+        var rePassword = $('#re-enter-password').val();
+
+        if(firstname === '' || lastname === '' || email === '' || password === '') {
+            $('form').submit(function (e) {
+                e.preventDefault();
+            })
+
+            var emptyFields = getEmptyFields();
+            var error = '<h4>The following fields remained empty:</h4><ul>';
+
+            for(var i = 0; i < emptyFields.length; i++) {
+                error += '<li>' + emptyFields[i] + '</li>';
+            }
+            error += '</ul>';
+            $('.error-message-field').html(error);
+        }else {
+            $('form').unbind('submit').submit();
+            $.ajax({
+                url: '/signup/create/',
+                type: 'POST',
+                data: {
+                    "firstname": firstname,
+                    "lastname": lastname,
+                    "email": email,
+                    "password": password,
+                    "rePassword": rePassword
+                }
+            })
+        }
+    })
+});
