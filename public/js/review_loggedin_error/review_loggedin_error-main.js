2d8b1f99e3cefd2b027b70ecaaee4a739fca69f0
goodGuyVictor <victor.kataev1@gmail.com>
2017-08-15 16:28:02 +0200

    review_loggedin_error ready

---
+++ public/js/review_loggedin_error/review_loggedin_error-main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Victor on 8/15/2017.
+ */
+$(document).ready(function() {
+    $('#login').on('click', function () {
+        var email = $('#email').val();
+        var password = $('#password').val();
+
+        $.ajax({
+            url: '/user/login/',
+            type: 'POST',
+            data: {"email": email, "password": password},
+            success: function() {
+                location.href = '/review/login/'
+            }
+        })
+    })
+});
