c92360ed3663f160fd9b4f01e69782db14e21471
goodGuyVictor <victor.kataev1@gmail.com>
2017-08-15 15:18:55 +0200

    created class submittedcontroller.php also created template submitted.tmpl

---
+++ simpleengine/controllers/submittedcontroller.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Victor
+ * Date: 8/15/2017
+ * Time: 12:16 PM
+ */
+
+namespace simpleengine\controllers;
+
+
+class SubmitController extends AbstractController
+{
+
+    public function actionIndex()
+    {
+        echo $this->render('submitted');
+    }
+}
