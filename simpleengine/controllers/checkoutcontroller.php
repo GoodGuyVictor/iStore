e8e86b10b98e9e94532d1fb92f4580d568d75468
goodGuyVictor <victor.kataev1@gmail.com>
2017-08-15 15:19:48 +0200

    implemented checkoutcontroller.php

--- simpleengine/controllers/checkoutcontroller.php
+++ simpleengine/controllers/checkoutcontroller.php
@@ -9,7 +9,31 @@
 namespace simpleengine\controllers;
 
 
-class CheckoutController
+use simpleengine\models\Authentication;
+use simpleengine\models\Order;
+
+class CheckoutController extends AbstractController
 {
 
+    public function actionIndex()
+    {
+        echo $this->render('checkout');
+    }
+
+    public function actionSetValues() {
+        session_start();
+        $_SESSION['amount'] = $_POST['amount'];
+    }
+
+    public function actionMakeNewOrder() {
+        session_start();
+        $auth = new Authentication($_SESSION['email']);
+        $userId = $auth->getIdByEmail();
+        $order = new Order($userId, $_SESSION['amount']);
+        try {
+            $order->makeNewOrder();
+        }catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+    }
 }
